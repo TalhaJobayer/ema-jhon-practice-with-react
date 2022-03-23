2ccb318f8721fbb40a7d86d386e255fcdb20cf4a
Talha Jobayer <talhajobayer@gmail.com>
2022-03-24 01:42:52 +0600

    cart complete without local storage

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import Product from '../Product/Product';
 import './Cart.css'
 
 const Carts = ({Cart}) => {
+    let total=0;
+    Cart.forEach(Price => {
+       total=total+Price.price;
+     
+        
+    }); 
+    const charge=Cart.length*10;
+    const tax=Math.ceil(total*.20)
+    const grandTotal=total+charge+tax
+     
     return (
-        <div>
+        <div className='full-container'>
              <h1>Order Summery</h1>
         <div className='text-part2'>
            
             <h3>Selected Items:{Cart.length}</h3>
-             <h3>Total Price:</h3>
-             <h3>Shipping charge:</h3>
-             <h3>Tax:</h3>
-             <h1>Grand Total:</h1>
+             <h3>Total Price:$ {total}</h3>
+             <h3>Shipping charge:$ {charge}</h3>
+             <h3>Tax:$ {tax}</h3>
+             <h1>Grand Total:$ {grandTotal}</h1>
         </div>
+        <button></button>
         </div>
     );
 };
